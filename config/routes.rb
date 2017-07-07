c716af8b85b13ee942cc371b0a3a4a309b3280a3
Neqo Dev <neqodev@iMac-de-CKS.local>
2017-07-07 18:07:27 +0200

    premier test de css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home' => 'pages#home'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  root 'pages#home'
   get 'admin' => 'pages#admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'books/:id' => 'books#show'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
+  end
